55aa7b134f195939a1fc602c65ea77939812f439
Nimzonimzo <91897827+Nimzonimzo@users.noreply.github.com>
2024-01-13 07:33:42 +0100

    Les pieces sont places en hauteur mais pas dans les angles...

--- src/controller/ChessboardController.java
+++ src/controller/ChessboardController.java
@@ -111,7 +111,7 @@ public class ChessboardController {
         imageView.setFitWidth(50);
         imageView.setFitHeight(50);
 
-        // Ajustez les coordonnées pour placer les pièces dans les coins supérieurs gauche
+        // Ajustez les coordonnées pour placer les pièces dans les coins supérieurs gauche mais pos aujourdhui card Disney
         piecesGrid.add(piecePane, col, row);
         GridPane.setRowIndex(piecePane, row);
         GridPane.setColumnIndex(piecePane, col);
